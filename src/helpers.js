486a5ee7c08fdb73a2dbe7831f3862bf6df6e25a
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-05-15 13:23:29 -0400

    Creates tests w/ spies

--- src/helpers.js
+++ src/helpers.js
@@ -2,11 +2,18 @@ const uuid = require('uuid');
 
 module.exports = {
   makePerson,
+  forEvenOnly,
 }
 
 function makePerson(first, last) {
   return {
     id: uuid(),
     name: `${first} ${last}`,
+  }
+}
+
+function forEvenOnly(num, cb) {
+  if(num % 2 === 0) {
+    cb(num);
   }
 }
--- src/helpers.test.js
+++ src/helpers.test.js
@@ -2,6 +2,7 @@ const helpers = require('./helpers.js');
 
 // callback that returns a function
 // jest.mock('uuid', () => () => '1234');
+// ^^^^^ replaced w/ uuid.js in __mocks__
 
 describe('helpers', () => {
   describe('makePerson()', () => {
@@ -9,6 +10,23 @@ describe('helpers', () => {
       const expected = { id: '1234', name: 'Frodo Baggins' };
       const actual = helpers.makePerson('Frodo','Baggins');
       expect(actual).toEqual(expected);
+    })
+  })
+
+  describe('forEvenOnly()', () => {
+    it('should invoke callback when given an even number', () => {
+      const spy = jest.fn();
+      helpers.forEvenOnly(2, spy);
+      helpers.forEvenOnly(4, spy);
+      expect(spy).toHaveBeenCalledTimes(2);
+      expect(spy).toHaveBeenNthCalledWith(1,2);
+      expect(spy).toHaveBeenNthCalledWith(2,4);
+    })
+
+    it('should NOT invoke callback when given an odd number', () => {
+      const spy = jest.fn();
+      helpers.forEvenOnly(3, spy);
+      expect(spy).not.toHaveBeenCalled();
     })
   })
 })
